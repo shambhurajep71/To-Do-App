d40ec2fb0aaaf9cbbb32a61858f6874b86bf956d
shambhurajep71 <shambhurajepatil71@gmail.com>
2024-08-06 00:21:15 +0530

    created navbar, added functionality to create new task & created ticket layout

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-
-  
+const createNewTask = document.querySelector(".create_new_task_btn");
+const createNewTicket = document.querySelector(".create_new_ticket");
+const cancelNewTask= document.querySelector(".cancel");
+// creating New task using Nav bar button;
+createNewTask.addEventListener("click", () => {
+    createNewTicket.style.display = "flex";
+});
+// cancel creation for ne task
+cancelNewTask.addEventListener("click", () => {
+    createNewTicket.style.display = "none";
+});
